084d5b477626d6ee04291f9d7e78fb2f30c86d68
rishi-0205 <rishijc02@gmail.com>
2024-06-28 23:01:56 +0530

    Added suspense

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -10,12 +10,14 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
 
       <div className="mt-10 prompt_layout">
         {data.map((post) => (
-          <PromptCard
-            key={post._id}
-            post={post}
-            handleEdit={() => handleEdit && handleEdit(post)}
-            handleDelete={() => handleDelete && handleDelete(post)}
-          />
+          <React.Suspense>
+            <PromptCard
+              key={post._id}
+              post={post}
+              handleEdit={() => handleEdit && handleEdit(post)}
+              handleDelete={() => handleDelete && handleDelete(post)}
+            />
+          </React.Suspense>
         ))}
       </div>
     </section>
